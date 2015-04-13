52b2f45d85ec149ca0a835dbc722c1191d426e5c
vartan <michael.vartan@student.csulb.edu>
2015-04-13 13:52:37 -0700

    documentation

--- MessageHandler.js
+++ MessageHandler.js
@@ -1,10 +1,10 @@
 "use strict";
 var serialport = require("serialport");
 var events = require("events");
-var util = require('util');
+var util = require("util");
 var Q = require("q");
 
-
+/** @type {Number} Number of bytes which describe command */
 var MESSAGE_ID_BYTES = 1;
 
 
@@ -13,74 +13,49 @@ var MESSAGE_ID_BYTES = 1;
  * @param {Object} info  Object including ID, name, length
  */
 function MessageHandler(info) {
-    var id = info.id || 0;
-    var name = info.name || "Unnamed Message"
-    var length = info.length || 1;
+  var id = info.id || 0,
+      name = info.name || "Unnamed Message",
+      length = info.length || 1;
+
+  events.EventEmitter.call(this);
 
 
-    if(id < 0 || id > Math.pow(2,8*MESSAGE_ID_BYTES)) {
-        throw new Error("Message #"+id+" \""+name+"\" does not fit inside of "+ 
-            "of "+MESSAGE_ID_BYTES+" bytes.");
-    }
-    this.id = id;
-    this.name = name;
-    this.length = length;
+  if(id < 0 || id > Math.pow(2, 8 * MESSAGE_ID_BYTES)) {
+    throw new Error("Message #" + id + " \"" + name + "\" does not fit" +
+      " inside of " + MESSAGE_ID_BYTES + " bytes.");
+  }
+  this.id = id;
+  this.name = name;
+  this.length = length;
 
 }
 util.inherits(MessageHandler, events.EventEmitter);
 
 /**
- * Receive Promise
+ * Receive Promise 
  * @return {Promise} Promise for receiving the message
  */
 MessageHandler.prototype.receive = function() {
+  // required to wrap this function because the scope called by q isn't
+  // the MessageHandler scope.
   var that = this;
-  return function() {
+  return function () {
     var deferred = Q.defer();
-
-    that.once("message", function(data) {
+    that.once("message", function receiveQCallback(data) {
       deferred.resolve(data);
     });
-    //that.pendingDeferred.push(deferred);
-
     return deferred.promise;
-  }
-}
-  
-module.exports = function(serialFile) {
-  var message_handlers = [];
+  };
+};
 
-  /**
-   * Variable length message parser
-   * @return {parser} Parses the messages to be dispatched 
-   */
-  var data = new Buffer(0);
-  var length = MESSAGE_ID_BYTES;
-  var messageHandler = undefined;
-  var variableLengthMessageParser = function(emitter, buffer){
-      data = Buffer.concat([data, buffer]);
-      while (data.length >= length) {
-        var out = data.slice(0,length)
-        data = data.slice(length);
+module.exports = function(serialFile, serialSettings) {
+  var message_handlers = [],
+      data = new Buffer(0),
+      length = MESSAGE_ID_BYTES,
+      messageHandler,
+      serialPort,
+      openPromise;
 
-        if(messageHandler === undefined) {
-          messageHandler = getHandler(out[0]);
-          if(messageHandler) {
-            length = messageHandler.length;
-            messageHandler.emit('data', out);
-          }
-
-        } else {
-          messageHandler.emit("message", out);
-          messageHandler.emit('data', out);
-
-          length = MESSAGE_ID_BYTES;
-          messageHandler = undefined;
-        }
-      }
-      emitter.emit('data', buffer);
-
-    };
   /**
    * Add handler generator
    *
@@ -89,7 +64,7 @@ module.exports = function(serialFile) {
    * @param {[type]} name    [description]
    * @param {[type]} handler [description]
    */
-  var addHandler = function (info) {
+  function addHandler(info) {
       var handler = new MessageHandler(info);
       message_handlers[handler.id] = handler;
       return handler;
@@ -99,27 +74,48 @@ module.exports = function(serialFile) {
    * @param  {Number}      id Handler ID
    * @return {MessageHandler} Handler for the ID
    */
-  var getHandler = function(id) {
+  function getHandler(id) {
       if(message_handlers.hasOwnProperty(id)) {
           return message_handlers[id];
-      } else {
-          //throw Error("Handler #"+id+" doesn't exist.")
-          return undefined;
       }
+      return undefined;
+  }
+  /**
+   * Variable length message parser
+   * @return {parser} Parses the messages to be dispatched 
+   */
+  function variableLengthMessageParser(emitter, buffer) {
+    var out;
+    data = Buffer.concat([data, buffer]);
+    while (data.length >= length) {
+      out = data.slice(0,length);
+      data = data.slice(length);
+
+      if(messageHandler === undefined) {
+        messageHandler = getHandler(out[0]);
+        if(messageHandler) {
+          length = messageHandler.length;
+          messageHandler.emit("data", out);
+        }
+
+      } else {
+        messageHandler.emit("message", out);
+        messageHandler.emit("data", out);
+
+        length = MESSAGE_ID_BYTES;
+        messageHandler = undefined;
+      }
+    }
+    emitter.emit("data", buffer);
+
   }
 
-  var serialPort = new serialport.SerialPort(serialFile, {
-    baudRate: 9600,
-    parser: variableLengthMessageParser
-  }, false); 
-
   /**
-   * Open serial via promise
-   * @param  {SerialPort} serial Serial port to open
-   * @return {Promise}    
+   * Open Serial
+   * @param  {SerialPort} node-serialport object     
+   * @return {Promise}    promise when it is opened
    */
-  var openPromise;
-  var openSerial = function(serial) {
+  function openSerial(serial) {
     // defer return to promise
     var deferred = Q.defer();
     // asynchronous serial open
@@ -135,29 +131,47 @@ module.exports = function(serialFile) {
     // return promise
     return deferred.promise;
   }
-  var sendMessage = function(message) {
+  /**
+   * Send Message over serial
+   * @param  {String || Buffer} message String/buffer to send over serialport
+   * @return {Promise}          Promise which will resolve when the message is
+   *                            finished sending
+   */
+  function sendMessage(message) {
     return function() {
       // defer return to promise
       var deferred = Q.defer();
       // asynchronous serial write
       serialPort.write(message, function(err,results) {
-          if(err)
-            deferred.reject(error);
-          else
+          if(err) {
+            deferred.reject(err);
+          } else {
             deferred.resolve(results);
+          }
       });
       // return promise
       return deferred.promise;
-    }
+    };
   }
+
+  serialSettings = serialSettings || {
+    baudRate: 9600,
+    parser: variableLengthMessageParser
+  };
+  
+  serialPort = new serialport.SerialPort(serialFile, serialSettings, false); 
+
+  // Automatically open serial port
+  // May consider making this optional later.
   openSerial(serialPort);
+
   return {
     MessageHandler    : MessageHandler,
     addHandler        : addHandler,
     getHandler        : getHandler,
-    open              : function(){return openPromise},
+    open              : function() { return openPromise; },
     serialPort        : serialPort,
     sendMessage       : sendMessage,
     write             : serialPort.write
   };
-}
+};
--- run.js
+++ run.js
@@ -2,54 +2,112 @@
 "use strict";
 var Q = require("q");
 
+// Open a new message handler
+var serialIdentifier = process.argv[2] || "/dev/tty.usbserial-A7027DGF";
+var messageHandler = require("./MessageHandler.js")(serialIdentifier);
 
-var messageHandler = require("./MessageHandler.js")("/dev/tty.usbserial-A7027DGF");
-var serialPort = messageHandler.serialPort;
-function logMessageHandler(data){
-  console.log(this.name+": every time receive: " + data);
-}
+// echo all received characters back to command line
+messageHandler.serialPort.on("data", echo);
 
-var ascii = function(char){return char.charCodeAt(0)};
-
+// Add message with ID 0x61 (ASCII 'a'). 
 var testHandler = messageHandler.addHandler({
   id:       ascii('a'),
   name:     "'a' handler", 
-  length:   4, 
-}).on("message", logMessageHandler);
+  length:   4, // payload length is 4, therefore the 4 characters sent after 'a'
+               // get sent to the message handler(s)
+}).on("message", logMessageHandler).on("message", sendNewLine);
 
+// Add message with ID 0x62 (ASCII 'b')
 var testHandler2 = messageHandler.addHandler({
   id:       ascii('b'), 
-  name:     "'b' handler", 
+  name:     "'b' handler",
   length:   5, 
-}).on("message", logMessageHandler);
+}).on("message", logMessageHandler).on("message", sendNewLine);
 
-var echo = function(data) {
-  for(var i=0;i<data.length;i++) {
-    var char = data[i];
-    if(char==="\r".charCodeAt(0))
-      this.write("\r\n", function(){});
-    else if(char === 0x7f)
-      this.write("\b \b", function(){});
-    else 
-      this.write(String.fromCharCode(char),function(){})
+// Add message with ID 0x65 (ascii 'e')
+var testHandler3 = messageHandler.addHandler({
+  id:       ascii('e'), 
+  name:     "exit handler",
+  length:   3, 
+}).on("message", sendNewLine);
+
+// Add to message handler, if the payload is "xit", then send a newline, then
+// exit the program.
+testHandler3.on("message", function(data) {
+  if (data.toString() === "xit") {
+    sendNewLine().then(process.exit);
   }
-}
+});
 
-messageHandler.serialPort.on("data", echo);
-Q.when(messageHandler.open())
-  .then(messageHandler.sendMessage("test\r\n"))
-  .then(function(messageLength){
+
+
+
+//Example sequence of events:
+//1. ensure message handler is open
+//2. send "test" message
+//3. report how many characters were sent 
+//4. receive 'a' message
+//5. console log a message back to user
+//6. receive 'b' message.
+//7. console log b message back to user
+//8. log "finished"
+
+Q.when(messageHandler.open())                                                //1
+  .then(messageHandler.sendMessage("test\r\n"))                              //2
+  .then(function(messageLength) {                                            //3
     console.log("sent message which was "+messageLength+" characters long.");
   })
-  .then(testHandler.receive())
-  .then(function(data) {
-    console.log("Finally received expected message 1: "+data);
+  .then(testHandler.receive())                                               //4
+  .then(function(data) {                                                     //5
+    console.log("Finally received expected message 1: "+data);               
   })
-  .then(testHandler2.receive())
-  .then(function(data) {
-    console.log("Finally received expected message 2: "+data);
-  })
-  .then(function(){
-    process.exit();
+  .then(testHandler2.receive())                                              //6
+  .then(function(data) {                                                     //7
+    console.log("Finally received expected message 2: "+data);            
+  })  
+  .finally(function() {                                                      //8
+    console.log("Finished!");
   })
   .catch(function(error){console.log(error);});
+
+
+/*********************** Helper Functions Below *************************/
+
+/**
+ * Log received messages to command line, and notify user via bell.
+ * @param  {Buffer} data Data received over serial
+ */
+function logMessageHandler(data) {
+  console.log("(" + this.name + ": " + data + ")\u0007");
+}
+
+/**
+ * Used to echo received data back over serial.
+ * @param  {Buffer} data Data being echoed back
+ */
+function echo(data) {
+  var i;
+  for (i = 0; i < data.length; i += 1) {
+    var char = data[i];
+    if (char === "\r".charCodeAt(0)) {
+      this.write("\r\n", function(){});
+    } else if (char === 0x7f) {
+      this.write("\b \b", function(){});
+    } else {
+      this.write(String.fromCharCode(char),function(){});
+    }
+  }
+}
+/**
+ * Send New Line
+ * Sends a line break over serial, for debugging purposes
+ * @return {Promise} Promise to send new newline character
+ */
+function sendNewLine() {
+  return Q.fcall(messageHandler.sendMessage("\r\n")); 
+}
+
+function ascii (char){
+  return char.charCodeAt(0);
+}
+
