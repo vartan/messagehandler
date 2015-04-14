89f62411008b4fcd8e66869a57f393a719294d01
vartan <michael.vartan@student.csulb.edu>
2015-04-14 09:05:04 -0700

    fixed require in example

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ var Q = require("q");
 
 // Open a new message handler
 var serialIdentifier = process.argv[2] || "/dev/tty.usbserial-A7027DGF";
-var messageHandler = require("serialmessages")(serialIdentifier);
+var messageHandler = require("./serialmessages.js")(serialIdentifier);
 
 // echo all received characters back to command line
 messageHandler.serialPort.on("data", echo(messageHandler.serialPort));
